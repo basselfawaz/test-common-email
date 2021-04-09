09afc17110a785091fb2d265f844f876e23398b6
Bassel Fawaz <basel06_fawaz@hotmail.com>
2021-04-08 20:35:57 -0400

    AddBcc test

---
+++ commons-email-1.3.2-src/src/test/java/EmailTest.java
@@ -0,0 +1,34 @@
+package org.apache.commons.mail;
+
+import static org.junit.Assert.assertEquals;
+
+import java.sql.Date;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EmailTest {
+
+	private static final String[] TEST_EMAILS = {"cb@dc.com", "a.b@c.org", "abcdefghijklmnopqrst@abcdefghijklmnopqrst.com.bd"};
+	private String[] Test_Names = {"basel", "lulu", "hello"};
+	private String[] validChars = {" ", "a", "A", "\uc5ec", "0123456789", "01234567890123456789","\n" };
+	
+	private EmailConcrete email;
+	
+	@Before
+	public void setUpEmailTest() throws Exception {
+		
+		email = new EmailConcrete();
+	}
+	
+	@After
+	public void tearDownEmailTest() throws Exception {
+		
+	}
+
+	@Test
+	public void addBCCTest() throws Exception {
+		email.addBcc(TEST_EMAILS[1], "Basel");
+		assertEquals(1, email.getBccAddresses().size());
+	}
